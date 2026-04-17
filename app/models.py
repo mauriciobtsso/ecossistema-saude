250ca603babe9b78f37b056c231daeead1df96ed
mauriciobtsso <mauriciobtsso@gmail.com>
2026-04-17 11:43:19 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -102,7 +102,7 @@ class Usuario(db.Model, UserMixin):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    senha_hash = db.Column(db.String(128), nullable=False)
+    senha_hash = db.Column(db.String(512), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='cliente')
     
     empresa_id = db.Column(db.Integer, db.ForeignKey('empresas.id'), nullable=True)
